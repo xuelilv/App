6172ebc8f15235008a967a07eb8d3816fd6e246e
xueli <xllv@xtremeprog.com>
2018-02-05 15:55:45 +0800

    fix index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
     Image,
     View,
+    Text,
     FlatList
 } from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
@@ -9,6 +10,16 @@ import FocusView from './viewPager';
 
 class SectionList extends React.Component {
     render() {
+        return (
+            <View style={styles.scene}>
+                <FocusView />
+                <FlatList
+                data={[{key: 'a'}, {key: 'b'}]}
+                renderItem={({item}) => <Text>{item.key}</Text>}
+                />
+            </View>
+        );
+        
         
     }
 }
@@ -30,9 +41,15 @@ class PageIndex extends React.Component {
                     renderIcon={() => <Image source={require('./assets/images/icon/index.png')} style={styles.image}/>}
                     renderSelectedIcon={() => <Image source={require('./assets/images/icon/index.png')} style={styles.image}/>}
                     onPress={() => this.setState({ selectedTab: 'Home' })}>
-                        <View>
-                            <FocusView></FocusView>
-                        </View>
+                        {<SectionList />}
+                </TabNavigator.Item>
+                <TabNavigator.Item 
+                    selected={this.state.selectedTab === 'Setting'}
+                    title="Setting"
+                    renderIcon={() => <Image source={require('./assets/images/icon/settle_setting.png')} style={styles.image}/>}
+                    renderSelectedIcon={() => <Image source={require('./assets/images/icon/settle_setting.png')} style={styles.image}/>}
+                    onPress={() => this.setState({ selectedTab: 'Setting' })}>
+                        {<SectionList />}
                 </TabNavigator.Item>
             </TabNavigator>
         );
@@ -47,6 +64,11 @@ const styles = {
     image: {
         width: 26,
         height: 26
+    },
+    scene: {
+        position: 'absolute',
+        top: 0,
+        bottom: 52
     }
 };
 
