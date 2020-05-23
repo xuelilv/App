901e1276c505d01c585d33af16d5f6e72fcd03b7
xuelilv <892659377@qq.com>
2020-05-23 18:24:59 +0800

    Update common.js

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -159,6 +159,30 @@
                     return word.substring(0, 1).toUpperCase() + word.substring(1);
                 });
             },
+            /**
+             * @param nodes 树数据
+             * @param predicate  过滤方法
+             */
+             deal: function(nodes, predicate) {
+              // 如果已经没有节点了，结束递归
+              if (!(nodes && nodes.length)) {
+                return []
+              }
+
+              var newChildren = []
+              for (const node of nodes) {
+                if (predicate(node)) {
+                  // 如果节点符合条件，直接加入新的节点集
+                  newChildren.push(node)
+                  node.childList = this.deal(node.childList, predicate)
+                } else {
+                  // 如果当前节点不符合条件，递归过滤子节点，
+                  // 把符合条件的子节点提升上来，并入新节点集
+                  newChildren.push(...this.deal(node.childList, predicate))
+                }
+              }
+              return newChildren
+            },
             setHtmlFontSize: function() {
                 function setWidth() {
                     var width = document.documentElement.clientWidth;
